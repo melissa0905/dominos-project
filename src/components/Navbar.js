70d9d637d3956cec597e1ebbac63f5ed2cd4ca0a
melisabayramli <53854645+melissa0905@users.noreply.github.com>
2021-03-20 14:44:33 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,3 @@
-// import React, { Component } from 'react'
-// import { IoCartOutline } from "react-icons/io5";
-// import Logo from '../images/dominos.png'
-// import {Link} from "react-router-dom";
-//  class Navbar extends Component {
-//     render() {
-     
-//         return (
-//             <div className="nav" style={{backgroundColor : "black"}} >
-//                   <div className="container">
-//                         <div className="nav__left">
-//                         <Link to="/"><img src={Logo} style={{width:"16%"}}></img> </Link>
-//                         </div>
-//                         <div className="nav__right">
-//                         <Link to="/cart">
-//                             <div className="basket">
-//                             <IoCartOutline className="cart-icon"  />
-                            
-//                             </div>
-//                         </Link>
-//                         </div>
-                       
-//                   </div>
-                
-//             </div>
-//         )
-//     }
-// }
-// export default Navbar
 import React from 'react'
 import {Link} from "react-router-dom";
 import {useSelector} from "react-redux";
